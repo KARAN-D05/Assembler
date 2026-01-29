d54c01dae884d02ad77fa3f5fbce5ee1dd097cd7
Karan Diwan <karandiwan46@gmail.com>
2026-01-29 17:38:57 +0530

    Delete ARM directory

--- ARM/p1.s
+++
@@ -1,6 +0,0 @@
-.global _start // starting point of label
-_start: // label
-	
-	mov r0,#30 // #30 -> Decimal, Hex -> #0x0A
-	mov r7,#1 // r7 -> stores system interrupts, #1-> exit
-	swi 0 // software interrupt -> lets OS take over to read r7 and compare against a lookup table
