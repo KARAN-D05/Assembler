e4e77f268d7e037e2419fe18271916cec5b6ab62
Karan Diwan <karandiwan46@gmail.com>
2026-02-26 19:51:18 +0530

    Sample program1 Added

--- asm-v0/Sample-Programs/P1.asm
+++ asm-v0/Sample-Programs/P1.asm
@@ -1,5 +1,8 @@
-LOAD-0-1011 // asm-v0.3, No STORE mnemonic, abstracted away with LOAD-S-XXXX mnemonic
-LOAD-1-1100
-ADD
-FBK
-SUB
+// asm-v0.3, No STORE mnemonic, abstracted away with LOAD-S-XXXX mnemonic - Load Data on lines and also store them in the register.
+
+LOAD-0-0011 // Load 0011 in Register A
+LOAD-1-1000 // Load 1000 in Register B
+ADD // ADD Data in Register A and B
+FBK // Feedback output back to input (Register A)
+SUB // Subtract Data in Register A and B
+HOLD-OP // Hold operation/output for arbitrary number of clock cycles
