32ffe90bc2963d5ee4e52fe9490f98888d769e13
Karan Diwan <karandiwan46@gmail.com>
2026-02-26 20:10:44 +0530

    Sample program2 Added

--- asm-v0/Sample-Programs/P2.asm
+++ asm-v0/Sample-Programs/P2.asm
@@ -3,6 +3,6 @@ LOAD-1-0001 // Load 0001(8) in Register B
 ADD // ADD Data in Register A and B
 FBK // Feedback output back to input (Register A)
 SUB // Subtract Data in Register A and B
-FBK // Feedback output back to input (Register A)
-SUB // Subtract Data in Register A and B
+FBK
+SUB
 HOLD-OP // Hold operation/output for arbitrary number of clock cycles then back to Default ADD operation
