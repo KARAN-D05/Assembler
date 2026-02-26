1dc01c26f2c540bf546788b1186a1a957abe2ca1
Karan Diwan <karandiwan46@gmail.com>
2026-02-26 20:05:56 +0530

    Sample program1 Added

--- asm-v0/Sample-Programs/P1.asm
+++ asm-v0/Sample-Programs/P1.asm
@@ -1,8 +1,11 @@
-// asm-v0.3, No STORE mnemonic, abstracted away with LOAD-S-XXXX mnemonic - Load Data on lines and also store them in the register.
+// asm-v0.2, No STORE mnemonic, abstracted away with LOAD-S-XXXX mnemonic - Load Data on lines and also store them in the register.
+// asm-v0.2 follows Big-Endian format, hence XXXX corresponds to 1 2 4 8.
 
-LOAD-0-0011 // Load 0011 in Register A
-LOAD-1-1000 // Load 1000 in Register B
+LOAD-0-0110 // Load 0110(6) in Register A
+LOAD-1-0001 // Load 0001(8) in Register B
 ADD // ADD Data in Register A and B
 FBK // Feedback output back to input (Register A)
 SUB // Subtract Data in Register A and B
-HOLD-OP // Hold operation/output for arbitrary number of clock cycles
+HOLD-OP // Hold operation/output for arbitrary number of clock cycles then back to Default ADD operation
+
+// OUTPUT -> 0110(6) for FBK(A)-B, on output lines for HOLD-OP number of cycles, after that back to Default ADD operation as 01101(22).
