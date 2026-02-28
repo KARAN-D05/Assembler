9828586e9a9d3e43790d33a3576e1750eee7c78e
Karan Diwan <karandiwan46@gmail.com>
2026-02-28 13:44:20 +0530

    Sample program1 Added

--- asm-v0/Sample-Programs/P1.asm
+++ asm-v0/Sample-Programs/P1.asm
@@ -8,14 +8,16 @@
 
 // asm-v0.1 has seperate instructions for bringing Data on the Line (LOAD-S-XXXX) and then storing it (STORE)
 
-LOAD-0-1001 // Load 1001 on Data Lines
+LOAD-0-1001 // Load 1001(9) on Data Lines
 STORE       // Store data in Register A
-LOAD-1-1100 // Load 1100 on Data Lines
+LOAD-1-1100 // Load 1100(3) on Data Lines
 STORE       // Store data in Register B
 
 // asm-v0.2, No STORE mnemonic, abstracted away with LOAD-S-XXXX mnemonic - Load Data on lines and also store them in the register.
 
-LOAD-0-1001 // Load 1001 on Data Lines and Store in Register A
-LOAD-1-1100 // Load 1100 on Data Lines and store in Register B
+LOAD-0-1001 // Load 1001(9) on Data Lines and Store in Register A
+LOAD-1-1100 // Load 1100(3) on Data Lines and store in Register B
 
 OUTPUT -> This Program will load 1001 (9) in Register A and 1100(3) in Register B. At Output we will see A + B = 0011(12) as ADD is default Arithmetic Operation.
+
+Refer [asm-v0.1](../asm-v0.1)
