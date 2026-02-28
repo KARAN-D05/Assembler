a3667e4ab66c7cc1567e882082f549316f7b06ae
Karan Diwan <karandiwan46@gmail.com>
2026-02-28 15:25:20 +0530

    Sample program2 Added

--- asm-v0/Sample-Programs/P2.asm
+++ asm-v0/Sample-Programs/P2.asm
@@ -6,7 +6,8 @@
 // index  -> [3] [2] [1] [0]
 // weight ->  1   2   4   8
 
-// asm-v0.2, No STORE mnemonic, abstracted away with LOAD-S-XXXX mnemonic - Load Data on lines and also store them in the register.
+// Program follows asm-v0.2 standard, No STORE mnemonic, abstracted away with LOAD-S-XXXX mnemonic - Load Data on lines and also store them in the register.
+// Will Run in asm-v0.2 and asm-v0.3 standard assembler
 
 LOAD-0-0110 // Load 0110(6) in Register A
 LOAD-1-0001 // Load 0001(8) in Register B
@@ -16,3 +17,8 @@ SUB         // Subtract Data in Register A and B
 HOLD-OP     // Hold operation/output for arbitrary number of clock cycles then back to Default ADD operation
 
 # OUTPUT -> 0110(6) for FBK(A)-B, on output lines for HOLD-OP number of cycles, after that back to Default ADD operation as 01101(22).
+
+// It is advised to Use asm-v0.3 Standard Assembler:-
+// Features like Syntax and Semantic Analysis.
+// Output Error Statements indicating the error.
+// Compatible with asm-v0.2 standard.
