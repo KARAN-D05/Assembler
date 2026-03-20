0e47a5baa131f1031f64d7bf7b4e8cf913ae69d7
Karan Diwan <karandiwan46@gmail.com>
2026-03-20 13:38:33 +0530

    Linux/macOS Repository Downloader

--- download_repos.sh
+++ download_repos.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-# Linux/macOS Repository Downloader
+# Linux/macos Repository Downloader
 
 BASE_URL="https://github.com/KARAN-D05"
 RAW_URL="https://raw.githubusercontent.com/KARAN-D05"
