c8841623ab9c24cdc88e0ca78837a44b1aae0518
Karan Diwan <karandiwan46@gmail.com>
2026-05-30 14:51:46 +0530

    Sample Program5 Added

--- asm-v0/Sample-Programs/P5.asm
+++ asm-v0/Sample-Programs/P5.asm
@@ -128,8 +128,3 @@ Overflow condition was overridden.
 Computation integrity lost after OVRD.
 System running normally.
 ----------------------------------------------------
-
-// It is advised to Use asm-v0.3 Standard Assembler:-
-// Features like Syntax and Semantic Analysis.
-// Output Error Statements indicating the error.
-// Compatible with asm-v0.2 standard.
